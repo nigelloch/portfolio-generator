e172451bd9ee29076b873610c48d556ebca12b20
loch <nigelloch@gmail.com>
2022-01-05 15:38:35 -0600

    removed previous code

--- app.js
+++ app.js
@@ -3,15 +3,3 @@ console.log(profileDataArgs)
 
 
 
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i++) {
-
-    console.log(profileDataArr[i]);
-  }
-
-  console.log('============');
-
-  profileDataArr.forEach(profileItem => console.log(profileItem));
-};
-
-printProfileData(profileDataArgs);
