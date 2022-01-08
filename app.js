64b885107323a6b9ac80c69b6e1942b7bf46b30a
loch <nigelloch@gmail.com>
2022-01-08 15:00:11 -0600

    added stylsheet, completed generator

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
+const { writeFile, copyFile } = require('./utils/generate-site');
+
 const inquirer = require("inquirer");
 
-const fs = require("fs");
 const generatePage = require("./src/page-template");
 
 const promptUser = () => {
@@ -144,16 +145,19 @@ const promptProject = (portfolioData) => {
 
 promptUser()
   .then(promptProject)
-  .then((portfolioData) => {
-    console.log(portfolioData);
-
-    const pageHTML = generatePage(portfolioData);
-
-    fs.writeFile("./index.html", pageHTML, (err) => {
-      if (err) throw err;
-
-      console.log(
-        "Portfolio complete! Check out index.html to see the output!"
-      );
-    });
+  .then(portfolioData => {
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
   });
